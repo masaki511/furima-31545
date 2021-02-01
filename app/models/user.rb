a8caefd9ad5f8cb232008ae1bb0c11d3d8bb5901
masaki511 <74614174+masaki511@users.noreply.github.com>
2021-02-02 00:48:52 +0900

    ユーザー管理機能の修正_バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
   with_options presence: true do
-    NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
-    validates :lastname, format: { with: NAME_REGEX, message: 'には全角文字（漢字・ひらがな・カタカナ）を使用してください' }
-    validates :firstname, format: { with: NAME_REGEX, message: 'には全角文字（漢字・ひらがな・カタカナ）を使用してください' }
-    NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/.freeze
-    validates :lastname_kana, format: { with: NAME_KANA_REGEX, message: 'には全角カタカナを使用してください' }
-    validates :firstname_kana, format: { with: NAME_KANA_REGEX, message: 'には全角カタカナを使用してください' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字（漢字・ひらがな・カタカナ）を使用してください' } do
+      validates :lastname
+      validates :firstname
+    end
+    with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' } do
+      validates :lastname_kana
+      validates :firstname_kana
+    end
     validates :nickname
     validates :birthday
   end
