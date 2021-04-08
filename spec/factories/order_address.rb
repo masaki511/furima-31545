ef10c263294da96110e2ee82b14f5b4d352023f6
masaki511 <74614174+masaki511@users.noreply.github.com>
2021-04-08 20:09:45 +0900

    商品購入機能の修正_電話番号のダミーデータを文字列に変換

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
                                                                                               ).to_s).to_s
     end
     building_name { (Faker::Lorem.word + Faker::Number.between(from: 1, to: 5000).to_s).to_s }
-    phone_number { Faker::Number.number(digits: 11) }
+    phone_number { Faker::Number.number(digits: 11).to_s }
     token { ('tok_' + Faker::Lorem.characters(number: 11, min_alpha: 11) + Faker::Number.number(digits: 17).to_s).to_s }
   end
 end
