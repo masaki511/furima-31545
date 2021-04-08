b15ca242d49d625410c8fa2e5abe903f3e6a4c99
masaki511 <74614174+masaki511@users.noreply.github.com>
2021-04-08 17:56:29 +0900

    商品購入機能の修正_user,itemに関するテストコードを追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.build_stubbed(:item, image: fixture_file_upload('public/image/output-image.jpeg'))
       @order_address = FactoryBot.build(:order_address)
+      @order_address.user_id = @user.id
+      @order_address.item_id = @item.id
     end
 
     it 'すべての情報を適切に入力すると購入できる' do
@@ -58,6 +62,16 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.token = nil
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Token can't be blank")
+    end
+    it 'user_idが空だと購入できない' do
+      @order_address.user_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("User can't be blank")
+    end
+    it 'item_idが空だと購入できない' do
+      @order_address.item_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Item can't be blank")
     end
   end
 end
