b6fa9ac032c1cb8d810a6fb7d5c27fcdc9eb71ff
masaki511 <74614174+masaki511@users.noreply.github.com>
2021-04-08 20:07:42 +0900

    商品購入機能の修正_可読性の向上

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,12 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -38,5 +33,14 @@ class OrdersController < ApplicationController
 
   def move_to_items_index
     redirect_to root_path if (current_user.id == @item.user_id) || !@item.order.nil?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+    )
   end
 end
